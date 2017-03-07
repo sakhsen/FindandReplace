172daeb829cab84781d090333524b4fce5f66df6
Sakhsen <sakhsen@gmail.com>
2017-03-07 18:23:54 +0530

    Add Solution to Git

---
+++ FindReplace/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace FindReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Err = new System.Windows.Forms.ErrorProvider(this.components);
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Err)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory";
+            // 
+            // txtDir
+            // 
+            this.txtDir.AccessibleName = "Required";
+            this.txtDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDir.Location = new System.Drawing.Point(106, 43);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(486, 22);
+            this.txtDir.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(616, 41);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.AccessibleName = "Required";
+            this.txtFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFind.Location = new System.Drawing.Point(106, 69);
+            this.txtFind.Multiline = true;
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(486, 50);
+            this.txtFind.TabIndex = 2;
+            this.txtFind.Validating += new System.ComponentModel.CancelEventHandler(this.txtFind_Validating);
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.AccessibleName = "Required";
+            this.txtReplace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReplace.Location = new System.Drawing.Point(106, 125);
+            this.txtReplace.Multiline = true;
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(486, 50);
+            this.txtReplace.TabIndex = 3;
+            this.txtReplace.Validating += new System.ComponentModel.CancelEventHandler(this.txtReplace_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Find";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Replace";
+            // 
+            // Err
+            // 
+            this.Err.ContainerControl = this;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(616, 70);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(616, 99);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(3, 197);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(696, 55);
+            this.txtMessage.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(208, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Find && Replace text in all the sub folders";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 256);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find & Replace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Err)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider Err;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FindReplace/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace FindReplace
+{
+    public partial class Form1 : Form
+    {
+        string filter = "*.*";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter =  ConfigurationSettings.AppSettings["filter"].ToString();
+            ClearControls();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = FBD.SelectedPath;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearControls();
+        }
+
+        private void ClearControls()
+        {
+            txtDir.Text = "";
+            txtFind.Text = "";
+            txtReplace.Text = "";
+            txtMessage.Text = "";
+            btnBrowse.Focus();
+        }
+
+        //private void txtDir_Validating(object sender, CancelEventArgs e)
+        //{
+        //    if (txtDir.Text == "")
+        //        Err.SetError(txtDir, txtDir.AccessibleName);
+        //    else
+        //        Err.SetError(txtDir, "");
+        //}
+
+        private void txtFind_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtFind.Text == "")
+                Err.SetError(txtFind, txtFind.AccessibleName);
+            else
+                Err.SetError(txtFind, "");
+        }
+
+        private void txtReplace_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtReplace.Text == "")
+                Err.SetError(txtReplace, txtReplace.AccessibleName);
+            else
+                Err.SetError(txtReplace, "");
+        }
+
+        private bool TestValidating()
+        {
+            bool res = true;
+
+            if (txtDir.Text == "")
+            {
+                Err.SetError(txtDir, txtDir.AccessibleName);
+                res = false;
+            }
+            else
+                Err.SetError(txtDir, "");
+
+            if (txtFind.Text == "")
+            {
+                Err.SetError(txtFind, txtFind.AccessibleName);
+                res = false;
+            }
+            else
+                Err.SetError(txtFind, "");
+
+            if (txtReplace.Text == "")
+            {
+                Err.SetError(txtReplace, txtReplace.AccessibleName);
+                res = false;
+            }
+            else
+                Err.SetError(txtReplace, "");
+
+            return res;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (TestValidating())
+            {
+                string[] files = Directory.GetFiles(txtDir.Text, filter, SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    try
+                    {                        
+                        string contents = File.ReadAllText(file);  
+                                             
+                        contents = contents.Replace(txtFind.Text, txtReplace.Text);
+                        // Make files writable
+                        File.SetAttributes(file, FileAttributes.Normal);
+                        File.WriteAllText(file, contents, Encoding.Unicode);                        
+                    }
+                    catch (Exception ex)
+                    {
+                        txtMessage.Text += ex.Message;                      
+                    }
+                }
+                txtMessage.Text = "Replaced text in " + files.Count() + " file(s).";
+            }
+        }
+
+    }
+}
